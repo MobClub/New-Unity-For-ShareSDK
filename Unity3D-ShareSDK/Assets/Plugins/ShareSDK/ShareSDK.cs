1569cb52e4ab7293e13b661a85868bfd58e3b526
DangerousDON <chenjd@chenjddeMac-mini.local>
2016-05-03 11:35:30 +0800

    Fixed
    Fixed

--- Unity3D-ShareSDK/Assets/Plugins/ShareSDK/ShareSDK.cs
+++ Unity3D-ShareSDK/Assets/Plugins/ShareSDK/ShareSDK.cs
@@ -60,7 +60,7 @@ namespace cn.sharesdk.unity3d
 			#elif UNITY_IPHONE
 			shareSDKUtils = new iOSImpl(gameObject);
 			#endif
-			shareSDKUtils.InitSDK(null);
+			shareSDKUtils.InitSDK(appKey);
 			shareSDKUtils.SetPlatformConfig(platformConfigs);
 		}
 		
