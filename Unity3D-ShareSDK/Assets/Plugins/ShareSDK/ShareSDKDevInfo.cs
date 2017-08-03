7e81079d25013db1adc753c72867d7cf4d73ef47
yhxjw86 <yhxjw86@gmail.com>
2017-08-03 20:40:40 +0800

    跟换FacebookAPPKey
    修改Facebook的APPKey

--- Unity3D-ShareSDK/Assets/Plugins/ShareSDK/ShareSDKDevInfo.cs
+++ Unity3D-ShareSDK/Assets/Plugins/ShareSDK/ShareSDKDevInfo.cs
@@ -76,7 +76,7 @@ namespace cn.sharesdk.unity3d
 		public string AppKey = "568898243";
 		public string AppSecret = "38a4f8204cc784f81f9f0daaf31e02e3";
 		public string RedirectUrl = "http://www.sharesdk.cn";
-		public bool isNewApi = true;
+		public bool ShareByWebApi  = true;
 		public bool ShareByAppClient = false;
 		#elif UNITY_IPHONE
 		public const int type = (int) PlatformType.SinaWeibo;
@@ -195,9 +195,9 @@ namespace cn.sharesdk.unity3d
 		#if UNITY_ANDROID
 		public string SortId = "8";
 		public const int type = (int) PlatformType.Facebook;
-		public string ConsumerKey = "107704292745179";
-		public string ConsumerSecret = "38053202e1a5fe26c80c753071f0b573";
-		public string RedirectUrl = "http://mob.com/";
+		public string ConsumerKey = "1412473428822331";
+		public string ConsumerSecret = "a42f4f3f867dc947b9ed6020c2e93558";
+		public string RedirectUrl = "https://mob.com/";
 		public bool ShareByAppClient = false;
 		#elif UNITY_IPHONE
 		public const int type = (int) PlatformType.Facebook;
