5ee6a9896fdbc8ccd5f4233ff05580a1f33bcbc2
1019238091 <1019238091@qq.com>
2019-04-29 15:44:24 +0800

    新增bdopen

---
+++ Android_Java_Demo/src/cn/sharesdk/demo/bdopen/BdEntryActivity.java
@@ -0,0 +1,15 @@
+package cn.sharesdk.demo.bdopen;
+
+import android.os.Bundle;
+import cn.sharesdk.douyin.bdopen.DouYinHandlerActivity;
+
+/**
+ * Created by xiangli on 2019/3/21.
+ */
+
+public class BdEntryActivity extends DouYinHandlerActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
